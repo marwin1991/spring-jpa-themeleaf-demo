78b95ec295c4567aee7887cb6f8eb24e16bfa09b
Peter <peter.zmilczak@gmail.com>
2022-01-09 16:03:33 +0100

    some fake dao approach to codecool shop sprint 2

--- src/main/java/com/codecool/ormdemo/dao/implemention/EmployeeDaoMem.java
+++ src/main/java/com/codecool/ormdemo/dao/implemention/EmployeeDaoMem.java
@@ -2,6 +2,7 @@ package com.codecool.ormdemo.dao.implemention;
 
 import com.codecool.ormdemo.dao.EmployeeDao;
 import com.codecool.ormdemo.model.Employee;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
